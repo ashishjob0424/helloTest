bbb208daa70ec559dd92031b24caf00108ac7816
ashish.madankar <ashishjob0424@gmail.com>
2016-06-17 12:33:49 +0530

    Changes Made for Testing

--- index.php
+++ index.php
@@ -5,7 +5,5 @@
  * Date: 17-06-2016
  * Time: 11:33
  */
-
-/*Remove from local system*/
 echo '<h1>This is a test index fil e After making a changes on server.</h1>';
 /*Please contineu on server*/
