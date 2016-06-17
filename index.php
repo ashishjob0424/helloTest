c11db7bd25a110cd0a977bdbd2135e2e76eb362f
ashishjob0424 <ashishjob0424@gmail.com>
2016-06-17 12:57:02 +0530

    Delete index.php
    Deleted

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: ashish.madankar
- * Date: 17-06-2016
- * Time: 11:33
- */
-echo '<h1>This is a test index fil e After making a changes on server.</h1>';
-/*Please contineu on server*/
